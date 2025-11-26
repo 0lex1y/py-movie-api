ce91c6c255a39e396051fd09346dc5bb1f4c61d6
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-11-26 19:33:06 +0200

    Fix serializer and views

--- cinema/views.py
+++ cinema/views.py
@@ -27,9 +27,9 @@ def movie_detail(request, pk):
     if request.method == 'GET':
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
+    elif request.method == 'PUT':
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
